e4338b5fae45b7c63ed25a944744e855988837cc
James Pelster <captainswag101@gmail.com>
2024-01-23 14:02:30 -0800

    Use newline characters instead of endl

--- Orbitersdk/samples/ProjectApollo/src_sys/SystemsFramework/Tester.cpp
+++ Orbitersdk/samples/ProjectApollo/src_sys/SystemsFramework/Tester.cpp
@@ -5,15 +5,15 @@ int main() {
 	SystemsFramework saturn_systems = SystemsFramework("SaturnSystems.cfg");
 	SystemsFramework lm_systems = SystemsFramework("LEMSystems.cfg");
 
-	std::cout << "Saturn Systems:" << std::endl;
+	std::cout << "Saturn Systems:" << "\n";
 	for (auto& pair : saturn_systems.Hydraulic) {
-		std::cout << std::get<0>(pair) << std::endl;
+		std::cout << std::get<0>(pair) << "\n";
 	}
 
 
-	std::cout << std::endl << "LM Systems:" << std::endl;
+	std::cout << std::endl << "LM Systems:" << "\n";
 	for (auto& pair : lm_systems.Hydraulic) {
-		std::cout << std::get<0>(pair) << std::endl;
+		std::cout << std::get<0>(pair) << "\n";
 	}
 
 	return 0;
