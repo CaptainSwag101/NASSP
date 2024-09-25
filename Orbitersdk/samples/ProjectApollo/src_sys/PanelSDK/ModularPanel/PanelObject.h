abf87d8c420a1d47770074e4d2f981d73c7299dd
James Pelster <captainswag101@gmail.com>
2024-09-25 12:43:54 -0700

    Begin work on modular panel system

---
+++ Orbitersdk/samples/ProjectApollo/src_sys/PanelSDK/ModularPanel/PanelObject.h
@@ -0,0 +1,57 @@
+/***************************************************************************
+  This file is part of Project Apollo - NASSP
+  Copyright 2024-2024 James Pelster
+
+  System & Panel SDK (SPSDK) - New Modular Panel System
+
+  Project Apollo is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  Project Apollo is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Project Apollo; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+  See http://nassp.sourceforge.net/license/ for more details.
+
+  **************************************************************************/
+
+#pragma once
+
+#include <OrbiterSdk.h>
+#include <string>
+
+class PanelObject {
+public:
+	explicit PanelObject(POINT pos2D, VECTOR3 pos3D, std::string textureFilename, std::string name);
+	~PanelObject();
+
+	virtual void Redraw2D();
+	virtual void RedrawVC();
+	virtual void ProcessMouse2D();
+	virtual void ProcessMouseVC();
+
+	POINT GetPos2D();
+	VECTOR3 GetPos3D();
+	int GetWidth2D();
+	int GetHeight2D();
+	SURFHANDLE GetDrawDestinationSurface();
+
+	std::string Name;
+
+protected:
+	SURFHANDLE TextureSource, DrawDestination;
+	/*
+	Position values represent the position on the underlying panel where this object is located.
+	They are not to be used when doing internal drawing in Redraw2D! Let the panel use those values to then
+	copy/blit our DrawDestination surface onto itself in the proper position.
+	*/
+	POINT Pos2D; VECTOR3 Pos3D;
+	int Width2D, Height2D;
+};
