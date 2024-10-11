ee81184b17889f12363a59b464dbd4e802c81b74
James Pelster <captainswag101@gmail.com>
2024-10-11 10:28:08 -0700

    Add 2nd pass to panel config parsing to find duplicate panel names

--- Orbitersdk/samples/ProjectApollo/src_sys/PanelSDK/ModularPanel/PanelBuilder.h
+++ Orbitersdk/samples/ProjectApollo/src_sys/PanelSDK/ModularPanel/PanelBuilder.h
@@ -5,10 +5,19 @@
 #include <string>
 #include <vector>
 
+struct PanelInfo {
+	std::optional<std::string> name, texture;
+	std::optional<int> width, height;
+	PanelNeighbors neighbors;
+	std::optional<double> fov_override, offset_x, offset_y, offset_z;
+};
+
 class PanelBuilder {
 public:
-	static std::vector<Panel> ParsePanelConfig(std::string configPath, bool topLevel = true);
+	static std::vector<Panel> LoadFromConfigFile(std::string configPath);
 
 private:
+	static std::vector<Panel> PanelInfoToObjects(std::string configPath, std::vector<PanelInfo>& info);
+	static std::vector<PanelInfo> ParsePanelInfo(std::string configPath, bool topLevel);
 	static void LogErrorMissingPanelKey(std::string configPath, int panelNum, std::string missingKey);
 };
